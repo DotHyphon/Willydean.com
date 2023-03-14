2fe5cd5487c05bd410e73d9bd6c76cc066fbca20
William Dean <William.dean93@gmail.com>
2023-03-14 18:28:03 +1100

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+a {
+    flex-basis: auto;
+}
+</style>
